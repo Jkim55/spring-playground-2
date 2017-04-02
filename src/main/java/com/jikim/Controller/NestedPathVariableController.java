10e7b3944a4ec56c2a032a25f9c8ec4b84f54f6d
Ji Kim <jkim55@gmail.com>
2017-04-02 17:11:48 -0600

    nested path variables implementation with passing tests

--- src/main/java/com/jikim/Controller/NestedPathVariableController.java
+++ src/main/java/com/jikim/Controller/NestedPathVariableController.java
@@ -1,5 +1,33 @@
 package com.jikim.Controller;
 
-public class NestedPathVariableController {
+import com.jikim.Service.TaskIds;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
+@RequestMapping("task/{taskId}/comments")
+public class NestedPathVariableController {
+    @GetMapping("/") // <-- will match /tasks/34/comments/
+    public String getComments(@PathVariable int taskId) {
+        return String.format("taskId is %d", taskId);
+    }
+
+    @GetMapping("/individual/{commentId}") // <-- will match /tasks/34/comments/9
+    public String getComment(@PathVariable int taskId, @PathVariable int commentId) {
+        return String.format("taskId is %d & commentId is %d", taskId, commentId);
+    }
+
+    @GetMapping("/map/{commentId}")
+    public String getPathVariablesAsAMap(@PathVariable Map pathVariables) {
+        return pathVariables.toString(); // {taskId=46, commentId=35}
+    }
+
+    @GetMapping("/object/{commentId}")
+    public String getPathVariablesFromObject(TaskIds ids) {
+        return String.format("taskId is %d; commentId is %d", ids.getTaskId(), ids.getCommentId());
+    }
 }
--- src/test/java/com/jikim/NestedPathVariableControllerTest.java
+++ src/test/java/com/jikim/NestedPathVariableControllerTest.java
@@ -1,4 +1,64 @@
 package com.jikim;
 
+import com.jikim.Controller.NestedPathVariableController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(NestedPathVariableController.class)
 public class NestedPathVariableControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getIndex() throws Exception {
+        int pId = 54;
+        RequestBuilder getRequest = MockMvcRequestBuilders.get(String.format("/task/%d/comments/", pId));
+
+        this.mvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().string("taskId is 54"));
+    }
+
+    @Test
+    public void getNestedPathVariableAsInd() throws Exception {
+        int pId = 54;
+        int cId = 2;
+        RequestBuilder getRequest = MockMvcRequestBuilders.get(String.format("/task/%d/comments/individual/%d", pId, cId));
+
+        this.mvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().string("taskId is 54 & commentId is 2"));
+    }
+
+    @Test
+    public void getNestedPathVariableAsMap() throws Exception {
+        int pId = 5;
+        int cId = 123;
+        RequestBuilder getRequest = MockMvcRequestBuilders.get(String.format("/task/%d/comments/map/%d", pId, cId));
+
+        this.mvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().string("{taskId=5, commentId=123}"));
+    }
+
+    @Test
+    public void getNestedPathVariablesAsObject() throws Exception {
+        int pId = 731;
+        int cId = 41;
+        RequestBuilder getRequest = MockMvcRequestBuilders.get(String.format("/task/%d/comments/object/%d", pId, cId));
+
+        this.mvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().string("taskId is 731; commentId is 41"));
+    }
 }
